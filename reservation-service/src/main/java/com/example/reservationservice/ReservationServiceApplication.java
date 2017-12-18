610f140d47293ba193161e8a8b4bb402ff98d318
tab <tabchanj@gmail.com>
2017-12-18 15:17:43 +0800

    wureka

--- reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ import java.util.stream.Stream;
 
 @RestController
 @Configuration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
     @Bean
