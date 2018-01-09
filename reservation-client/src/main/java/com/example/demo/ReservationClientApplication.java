6926a5cadb2559fde083c5174dbe7e0da3476f6a
tab <tabchanj@gmail.com>
2018-01-09 22:57:44 +0800

    paratrizedType

--- reservation-client/src/main/java/com/example/demo/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/example/demo/ReservationClientApplication.java
@@ -1,9 +1,20 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Collection;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -13,4 +24,29 @@ public class ReservationClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationClientApplication.class, args);
 	}
+}
+
+@RestController
+@RequestMapping("/reservations")
+class ReservationApiGatewayRestController{
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(method = RequestMethod.GET,value = "/names")
+    public Collection<String> getResrvationNames(){
+        ParameterizedType<Resource<Reservation>> ptr = new ParamterizedType<Resource<Reservation>>(){};
+        ResponseEntity<Resource<Reservation>> entity =
+                this.restTemplate.exchange("http://reservation-service/reservations", HttpMethod.GET,null,ptr);
+    }
+}
+class Reservation{
+    private String reservationName;
+
+    public String getReservationName() {
+        return reservationName;
+    }
+
+    public void setReservationName(String reservationName) {
+        this.reservationName = reservationName;
+    }
 }
