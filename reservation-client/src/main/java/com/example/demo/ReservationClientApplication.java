b1a304e7d3e3336d02fa0e32ba8ef368c342e858
tab <tabchanj@gmail.com>
2018-02-02 16:10:55 +0800

    enable zuul fallbackprovider

--- reservation-client/src/main/java/com/example/demo/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/example/demo/ReservationClientApplication.java
@@ -1,46 +1,39 @@
 package com.example.demo;
 
+import com.netflix.hystrix.exception.HystrixTimeoutException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @EnableZuulProxy
+//@EnableCircuitBreaker
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationClientApplication {
-    @Bean
+    /*@Bean
     @LoadBalanced   //add this anotation to fix the problem that "service id(spring.application.name) can not be recognized"
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
-    }
-
-    @Bean
-    CommandLineRunner dc(DiscoveryClient dc) {
-        return args ->
-                dc.getInstances("reservation-service")
-                        .forEach(si -> System.out.println(
-                                si.getHost() + ':' + si.getPort()));
     }
 
     @Bean
@@ -59,19 +52,23 @@ public class ReservationClientApplication {
     }
 
     @Bean
+    CommandLineRunner dc(DiscoveryClient dc) {
+        return args ->
+                dc.getInstances("reservation-service")
+                        .forEach(si -> System.out.println(
+                                si.getHost() + ':' + si.getPort()));
+    }
+
+
+    @Bean
     CommandLineRunner feign(ReservationRestClient client) {
         return args ->
                 client.getReservations().forEach(System.out::println);
-    }
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationClientApplication.class, args);
     }
-}
-
-@Component
-class RestTemp {
-    public RestTemplate restTemplate = new RestTemplate();
 }
 
 @FeignClient("reservation-service")
@@ -128,5 +125,63 @@ class Reservation {
 
     public void setName(String name) {
         this.name = name;
+    }
+}
+
+@Component
+class MyFallbackProvider implements FallbackProvider {
+
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(final Throwable cause) {
+        if (cause instanceof HystrixTimeoutException) {
+            return response(HttpStatus.GATEWAY_TIMEOUT);
+        } else {
+            return fallbackResponse();
+        }
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return response(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ClientHttpResponse response(final HttpStatus status) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return status;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return status.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return status.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("sevice-down".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
     }
 }
