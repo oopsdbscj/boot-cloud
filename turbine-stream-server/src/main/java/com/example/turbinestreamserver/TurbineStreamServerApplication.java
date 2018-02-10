4f71dda403a6cc498ecb10b1dc3d093254f86a32
tab <tabchanj@gmail.com>
2018-02-10 15:11:35 +0800

    >>Exception: turbin stream work with RabbitMQ not success under spring cloud Edgware.SR2 >>resolve: change the spring cloud version to Edgware.SR2

--- turbine-stream-server/src/main/java/com/example/turbinestreamserver/TurbineStreamServerApplication.java
+++ turbine-stream-server/src/main/java/com/example/turbinestreamserver/TurbineStreamServerApplication.java
@@ -2,15 +2,8 @@ package com.example.turbinestreamserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
-import org.springframework.cloud.netflix.turbine.stream.TurbineStreamConfiguration;
-import org.springframework.cloud.netflix.turbine.stream.TurbineStreamProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import rx.subjects.PublishSubject;
 
 @Configuration
 @SpringBootApplication
